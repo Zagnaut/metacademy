7b1112331fcb7b84a434dcb6f80aa22b94647f16
Michael Patella <m.c.patella@gmail.com>
2013-05-24 19:39:17 -0400

    Major UI steps. Added Popovers for abilities on hover. Expanded upon protips and matchups

---
+++ public/js/protipsFactory.js
@@ -0,0 +1,31 @@
+app.factory("Protips", function() {
+  var protips = [];
+  protips = [
+    {
+      "user": "Zagnaut",
+      "upvotes": "4",
+      "description": "Mundo goes where he pleases",
+      "content": "He's a meat stick of course.",
+      "comments": []
+    },{
+      "user": "Dyrus",
+      "upvotes": "768",
+      "description": "That's so Darius. Here's a really long description that breaks line length. fix dat shit",
+      "content": "",
+      "comments": ""
+    },{
+      "user": "Scarra",
+      "upvotes": "9999",
+      "description": "PRESS R TO KATARINA",
+      "content": "Spin 2 win brosef.",
+      "comments": ""
+    },{
+      "user": "Scarra",
+      "upvotes": "9999",
+      "description": "PRESS R TO KATARINA",
+      "content": "Spin 2 win brosef.",
+      "comments": ""
+    }
+  ];
+  return protips;
+});

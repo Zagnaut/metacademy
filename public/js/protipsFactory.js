4abff3af88023dab68c6ab4290dc6e201a143584
Michael Patella <m.c.patella@gmail.com>
2013-05-24 20:14:41 -0400

    Adjusted fake protip data

--- public/js/protipsFactory.js
+++ public/js/protipsFactory.js
@@ -10,21 +10,21 @@ app.factory("Protips", function() {
     },{
       "user": "Dyrus",
       "upvotes": "768",
-      "description": "That's so Darius. Here's a really long description that breaks line length. fix dat shit",
-      "content": "",
-      "comments": ""
+      "description": "That's so DARIUS.",
+      "content": "IF DEY BLEED, IMMA CLICK THEM AND THEY DIE. lol",
+      "comments": []
     },{
       "user": "Scarra",
       "upvotes": "9999",
       "description": "PRESS R TO KATARINA",
       "content": "Spin 2 win brosef.",
-      "comments": ""
+      "comments": []
     },{
-      "user": "Scarra",
+      "user": "Ocelote",
       "upvotes": "9999",
-      "description": "PRESS R TO KATARINA",
-      "content": "Spin 2 win brosef.",
-      "comments": ""
+      "description": "PRESS R MORE BROS",
+      "content": "Also works with, Garen, Darius, Karthus, and Kat.",
+      "comments": []
     }
   ];
   return protips;

fd990f787d156df451a2b83a414147695e1ca07f
Michael Patella <m.c.patella@gmail.com>
2013-05-25 23:33:09 -0400

    Various changes

--- public/js/main.js
+++ public/js/main.js
@@ -1,58 +1,80 @@
-var app = angular.module("metacademy", ["$strap.directives"]);
+var app = angular.module("metacademy", ["$strap.directives"])
+    .config(['$routeProvider', function($routeProvider) {
+        $routeProvider
+            .when("/versus", {templateUrl: "/partials/versus.html"})
+            .when("/versus/:player", {templateUrl: "/partials/versus.html"})
+            .when("/versus/:player/:opponent", {templateUrl: "/partials/versus.html"})
+            .otherwise({redirectTo: "/index"});
+    }]);
 
 app.factory("RemainingChampions", function(){
-  var newChamps = [
-    {
-      _id: 0,
-      name: "Lissandra",
-      title: "the Ice Witch",
-      imageUrl: "",
-      abilities: []
-    }
-  ]
-  return newChamps;
+    var newChamps = [
+        {
+            _id: 0,
+            name: "Lissandra",
+            title: "the Ice Witch",
+            imageUrl: "",
+            abilities: []
+        }
+    ]
+    return newChamps;
 });
 
-
 function ChampCtrl($scope, Champions) {
-  $scope.champions = Champions;
+    $scope.champions = Champions;
 
-  $scope.findChampionByName = function (name) {
-      for (var i = 0; i < $scope.champions.length; i++) {
-        if ($scope.champions[i].name === name) {
-          return $scope.champions[i];
-        }
-      }
-  }
-  $scope.change = function() {
-    $scope.champion = $scope.findChampionByName($scope.search);
-  }
+    $scope.findChampionByName = function (name) {
+            for (var i = 0; i < $scope.champions.length; i++) {
+                if ($scope.champions[i].name === name) {
+                    return $scope.champions[i];
+                }
+            }
+    }
+    $scope.change = function() {
+        $scope.champion = $scope.findChampionByName($scope.search);
+    }
 }
 
 function ProtipsCtrl($scope, Protips) {
-  $scope.protips = Protips;
+    $scope.protips = Protips;
+}
+
+function MatchupsCtrl($scope, Protips) {
+    $scope.protips = Protips;
 }
 
 app.directive("autoComplete", function($timeout){
-  return function(scope, elem, attrs) {
-    elem.autocomplete({
-      source: champNames,
-      select: function() {
-        $timeout(function() {
-          elem.trigger('input'); 
-        }, 0);
-      }
-    });
-  };
+    return function(scope, elem, attrs) {
+        elem.autocomplete({
+            source: champNames,
+            select: function() {
+                $timeout(function() {
+                    elem.trigger('input'); 
+                }, 0);
+            }
+        });
+    };
+});
+
+app.directive("champion", function() {
+    return  function(scope, elem, attrs) {
+        if (attrs.champion === "player") {
+            var header = "Player"
+            elem.header = header
+        } else if (attr.champion === "opponent") {
+            var header = "Opponent"
+            elem.x = header
+        }
+    }
 });
 
 var requestAllChampions = function() {
-  $http({method: "GET", url: "/api/champions"}).
-      success(function(data, status, headers, config) {
-        // $scope.champions = data;
-        console.log(data);
-      }).
-      error(function(data, status, headers, config) {
-        console.log(data);
-      });
+    $http({method: "GET", url: "/api/champions"}).
+            success(function(data, status, headers, config) {
+                // $scope.champions = data;
+                console.log(data);
+            }).
+            error(function(data, status, headers, config) {
+                console.log(data);
+            });
 }
--- public/js/protipsFactory.js
+++ public/js/protipsFactory.js
@@ -1,31 +1,31 @@
 app.factory("Protips", function() {
-  var protips = [];
-  protips = [
-    {
-      "user": "Zagnaut",
-      "upvotes": "4",
-      "description": "Mundo goes where he pleases",
-      "content": "He's a meat stick of course.",
-      "comments": []
-    },{
-      "user": "Dyrus",
-      "upvotes": "768",
-      "description": "That's so DARIUS.",
-      "content": "IF DEY BLEED, IMMA CLICK THEM AND THEY DIE. lol",
-      "comments": []
-    },{
-      "user": "Scarra",
-      "upvotes": "9999",
-      "description": "PRESS R TO KATARINA",
-      "content": "Spin 2 win brosef.",
-      "comments": []
-    },{
-      "user": "Froggen",
-      "upvotes": "9999",
-      "description": "PRESS R MORE BROS",
-      "content": "Also works with, Garen, Darius, Karthus, and Kat.",
-      "comments": []
-    }
-  ];
-  return protips;
+    var protips = [];
+    protips = [
+        {
+            "user": "Zagnaut",
+            "upvotes": "4",
+            "description": "Mundo goes where he pleases",
+            "content": "He's a meat stick of course.",
+            "comments": []
+        },{
+            "user": "Dyrus",
+            "upvotes": "768",
+            "description": "That's so DARIUS.",
+            "content": "IF DEY BLEED, IMMA CLICK THEM AND THEY DIE. lol",
+            "comments": []
+        },{
+            "user": "Scarra",
+            "upvotes": "9999",
+            "description": "PRESS R TO KATARINA",
+            "content": "Spin 2 win brosef.",
+            "comments": []
+        },{
+            "user": "Froggen",
+            "upvotes": "9999",
+            "description": "PRESS R MORE BROS",
+            "content": "Also works with, Garen, Darius, Karthus, and Kat.",
+            "comments": []
+        }
+    ];
+    return protips;
 });

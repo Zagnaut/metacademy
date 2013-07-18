2dc190b1067fcf7a6710d64c7813d44f365a80d2
Michael Patella <m.c.patella@gmail.com>
2013-07-18 05:09:28 -0400

    Inserted stubbed protips data for UI dev

--- client/app/scripts/controllers/ProtipsCtrl.js
+++ client/app/scripts/controllers/ProtipsCtrl.js
@@ -1,8 +1,20 @@
 app.controller("ProtipsCtrl", ["$scope", "$protips", function ($scope, $protips) {
-  $scope.protips = [];
+  $scope.protips = [
+    {
+      user: "Zagnaut",
+      description: "Spin 2 Win",
+      content: "Garen, Katarina, Karthus, Xin Zhao",
+      upvotes: 200,
+    },{
+      user: "Scarra",
+      description: "Press R 2 win",
+      content: "Garen, Katarina, Karthus, Xin Zhao",
+      upvotes: 90,
+    }
+  ];
 
-  $scope.load = function() {
-    $scope.protips = $protips.get();
+  $scope.load = function () {
+    $scope.protips = $protips.getPlayer();
   };
 
   $scope.post = function() {

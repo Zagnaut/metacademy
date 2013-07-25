18218288d44194e6d8fac5cfd2619c26a11515a5
Michael Patella <m.c.patella@gmail.com>
2013-07-25 17:15:43 -0400

    Champions provider now accepts callbacks

--- server/champions/index.js
+++ server/champions/index.js
@@ -1,18 +1,25 @@
-// This file should probably be async instead of sync
 var Champion = require('../models/champion.js');
 
 // Send a json response with all champions in the database.
-exports.getChampions = function () {
-    Champion.find(function(err, champions) {
-        if (err) { console.log(err); }
-        return champions;
+exports.getAll = function (callback) {
+    Champion.find({}, function(err, champions) {
+        if (err) {
+            return console.log(err);
+        }
+        callback(champions);
     });
 };
 
 // Accepts a champion name to
-exports.getChampion = function(name) {
-    Champion.find({"name": name}, function(err, champion) {
-        if (err) { console.log(err); }
-        return champion;
+exports.getByName = function(name, callback) {
+    var query = {
+        name: name
+    };
+
+    Champion.find(query, function(err, champion) {
+        if (err) {
+            return console.log(err);
+        }
+        callback(champion);
     });
 };
